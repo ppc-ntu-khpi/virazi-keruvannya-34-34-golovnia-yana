dc9c92ec21b6ade2feaecb4a0ec279e9813741ab
Golovnia Yana <55803221+34-golovnia-yana@users.noreply.github.com>
2020-04-27 09:04:07 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,29 @@
 package domain;
+/**
+ * Class with calculations
+ * @author Asus
+ *
+ */
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+	/**
+	 * @param lowerLimit
+	 * @param upperLimit
+	 * @return range
+	 */
+	public static int[] Calculate(int lowerLimit, int upperLimit) {
+		/**
+		 * create an array
+		 */
+		int[] range = new int[upperLimit - lowerLimit];
+		/**
+		 * variable stores index in array (0 to array size)
+		 */
+		int index = 0;
+		for (int i = lowerLimit; i < upperLimit; i++) {
+			range[index] = i;
+			index++; 
+		}
+		return range;
+	}
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,8 +5,14 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+    	int lowerLimit = 0;
+		int upperLimit = 4;
+		
+		int[] range = Exercise.Calculate(lowerLimit, upperLimit);
+		
+		System.out.println("Діапазон значень від " + lowerLimit + " до (не включно) " + upperLimit + ":");
+		for (int i = 0; i < range.length; i++) {
+			System.out.println("["+i+"] = " + range[i]);
+		}
     }
 }
