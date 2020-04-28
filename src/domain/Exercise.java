d3809bac23e4d6f208e24fce6508ea71ecdcb51d
Golovnia Yana <55803221+34-golovnia-yana@users.noreply.github.com>
2020-04-28 23:01:19 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,29 +1,39 @@
 package domain;
+
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Class with calculations
+ * 
  * @author Asus
  *
  */
-
 public class Exercise {
 	/**
+	 * Method that returns an array of prime numbers
+	 * 
 	 * @param lowerLimit
 	 * @param upperLimit
-	 * @return range
+	 * @return primeNumbers
 	 */
-	public static int[] Calculate(int lowerLimit, int upperLimit) {
+	public static List<Integer> Calculate(int lowerLimit, int upperLimit) {
 		/**
-		 * create an array
+		 * we create a collection for storing primes
 		 */
-		int[] range = new int[upperLimit - lowerLimit];
-		/**
-		 * variable stores index in array (0 to array size)
-		 */
-		int index = 0;
-		for (int i = lowerLimit; i < upperLimit; i++) {
-			range[index] = i;
-			index++; 
+		List<Integer> primeNumbers = new ArrayList<Integer>();
+		rangeLoop: for (int i = lowerLimit; i < upperLimit; i++) {
+			if (i == 0 || i == 1)
+				continue;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0)
+					continue rangeLoop;
+			}
+			primeNumbers.add(i);
 		}
-		return range;
+		/**
+		 * return the collection
+		 */
+		return primeNumbers;
 	}
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,14 +5,6 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-    	int lowerLimit = 0;
-		int upperLimit = 4;
-		
-		int[] range = Exercise.Calculate(lowerLimit, upperLimit);
-		
-		System.out.println("Діапазон значень від " + lowerLimit + " до (не включно) " + upperLimit + ":");
-		for (int i = 0; i < range.length; i++) {
-			System.out.println("["+i+"] = " + range[i]);
-		}
+		System.out.println(Exercise.Calculate(0, 20));
     }
 }
